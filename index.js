5460e37633165977dc6ebf4bee626c5a29a78298
Bhavesh.kumar <bhavesh.connect38@gmail.com>
2024-03-13 13:18:14 +0000

    adding contract data

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 import express from "express";
 import cors from "cors";
+import axios from "axios";
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 const app = express();
 app.use(cors());
 app.use(function (req, res, next) {
@@ -20,28 +22,59 @@ const io = new Server(server, {
   },
 });
 
+let conData = "";
 // const io = new Server(server);
 app.get("/", (req, res) => {
-  res.send("<h1>Hello World - Bhavesh Kumar 123</h1>");
+  res.send(conData);
 });
 
 let iterationCount = 0;
+let dataToSend = {};
+
 io.on("connection", (socket) => {
   console.log("A user conencted");
   socket.on("message", (msg) => {
     io.emit("message", msg);
   });
-  
-  
+
   setInterval(() => {
     iterationCount++;
     var randomNumber = parseInt(Math.random() * 100) + 1;
     if (randomNumber > 50) {
-      io.emit("message", randomNumber);
+      io.emit("message", conData);
     }
   }, 10);
 });
 
+const filterCentralData = (d1) => {
+  d1 = d1.replaceAll("\n", "");
+  d1 = d1.replaceAll("\\\\", "");
+
+  const allCentralData = d1.split(";");
+  conData = allCentralData[2];
+  conData = conData.slice(25, -2);
+  conData = conData.split(",");
+  let tempFilterData = [];
+  conData.forEach((item) => {
+    let tempItem = item.split(":")[0].trim();
+    tempFilterData.push(tempItem);
+  });
+  conData = tempFilterData;
+  console.log(conData);
+};
+const fetchContractsData = async () => {
+  try {
+    const response = await axios.get(
+      `https://insight.corp.hertshtengroup.com/technicals/brazilcontractsdata.js?v=${Math.random()}`
+    );
+    let data = response.data;
+    filterCentralData(data);
+  } catch (error) {
+    console.error("Error fetching data:", error.message);
+  }
+};
+
+fetchContractsData();
 server.listen(process.env.PORT || 3000, () => {
   console.log("Server is running");
 });
