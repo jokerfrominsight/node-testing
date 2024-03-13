43becd7b4c60e0529a37ae3a59be7504fcd4442b
Bhavesh.kumar <bhavesh.connect38@gmail.com>
2024-03-13 07:33:40 +0000

    adding cors policy

--- index.js
+++ index.js
@@ -2,12 +2,19 @@ import express from "express";
 import cors from "cors";
 const app = express();
 app.use(cors());
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  res.header("Access-Control-Allow-Methods", "PUT, GET, POST, DELETE, OPTIONS");
+  next();
+});
 import http from "http";
 import { Server } from "socket.io";
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "https://insight.corp.hertshtengroup.com/", // Specify the origin of your client
+    origin: "*", // Specify the origin of your client
     methods: ["GET", "POST"], // Specify the methods allowed
     // credentials: true, // Set to true if you're using credentials
   },
